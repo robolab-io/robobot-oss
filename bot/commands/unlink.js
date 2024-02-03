173c824608337893ff55d28c23f03a2dfb789309
Evan <hi@evan.graphics>
2024-02-02 21:42:53 -0700

    fix: option names can only be one word strings

--- bot/commands/unlink.js
+++ bot/commands/unlink.js
@@ -9,22 +9,22 @@ module.exports = {
     .setDescription("Request to unlink a MeckaKeys account from Discord.")
     .addUserOption((option) =>
       option
-        .setName("MechaKeys Username")
-        .setDescription("MechaKeys Username (case sensitive)")
+        .setName("username")
+        .setDescription("Your MechaKeys Username (case sensitive)")
         .setRequired(true)
     )
     .addIntegerOption((option) =>
       option
-        .setName("MechaKeys Account Email")
-        .setDescription("Accounts associated email to send verification")
+        .setName("email")
+        .setDescription("The email associated with your MechaKeys account")
         .setRequired(true)
     ),
 
   async execute(interaction) {
     await interaction.deferReply()
 
-    const MK_Username = interaction.options.getString("MechaKeys Username")
-    const MK_Account_Email = interaction.options.getString("MechaKeys Account Email")
+    const MK_Username = interaction.options.getString("username")
+    const MK_Account_Email = interaction.options.getString("email")
 
     let res = await devAPI.unlink(MK_Username, MK_Account_Email)
     console.log(`UNLINK REQUEST\nRequester: ${interaction.user.id}\nMK User:${MK_Username}\nRes: ${res}`)
